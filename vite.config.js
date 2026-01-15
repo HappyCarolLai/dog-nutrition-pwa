52ce61d4253c214dddd62a293c617988f61dd821
HappyCarolLai <x94u6up3@gmail.com>
2026-01-15 11:19:20 +0800

    告訴 Vite：所有建置後的資源都以 /dog-nutrition-pwa/ 為根目錄

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/dog-nutrition-pwa/',   // <-- 這行很重要
   plugins: [react()],
 })
