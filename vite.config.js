c42a086687d0df5b41485b2395393126df37ea19
HappyCarolLai <x94u6up3@gmail.com>
2026-01-15 13:02:53 +0800

    解決404錯誤

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/dog-nutrition-pwa/',   // <-- 這行很重要
+  base: '/dog-nutrition-pwa/',
   plugins: [react()],
 })
